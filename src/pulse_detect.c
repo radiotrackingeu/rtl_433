6a7cbdcbabcf35c24b85f78e6ff9e510befdbc39
ralf <ralf.zeidler@fridata.de>
2017-07-24 18:16:00 +0200

    hsyt / 2

--- src/pulse_detect.c
+++ src/pulse_detect.c
@@ -219,7 +219,7 @@ int pulse_detect_package(const int16_t *envelope_data, const int16_t *fm_data, i
 		const int16_t am_n = envelope_data[s->data_counter];
 		int16_t ook_threshold = s->ook_low_estimate + (s->ook_high_estimate - s->ook_low_estimate) / 2;
 		if (level_limit != 0) ook_threshold = level_limit;	// Manual override
-		const int16_t ook_hysteresis = ook_threshold / 8;	// ±12%
+		const int16_t ook_hysteresis = ook_threshold / 2;	// ±12%
 
 		// OOK State machine
 		switch (s->ook_state) {

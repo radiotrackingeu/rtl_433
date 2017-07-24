e32195caf72b530c5a2a930ca53641363b002e79
ralf <ralf.zeidler@fridata.de>
2017-07-24 17:00:18 +0200

    times 2

--- src/pulse_detect.c
+++ src/pulse_detect.c
@@ -32,12 +32,12 @@ void pulse_data_print(const pulse_data_t *data) {
 
 
 // OOK adaptive level estimator constants
-#define OOK_HIGH_LOW_RATIO	8			// Default ratio between high and low (noise) level
+#define OOK_HIGH_LOW_RATIO	8*2			// Default ratio between high and low (noise) level
 #define OOK_MIN_HIGH_LEVEL	500			// Minimum estimate of high level
 #define OOK_MAX_HIGH_LEVEL	(128*128)	// Maximum estimate for high level (A unit phasor is 128, anything above is overdrive)
 #define OOK_MAX_LOW_LEVEL	(OOK_MAX_HIGH_LEVEL/2)	// Maximum estimate for low level
-#define OOK_EST_HIGH_RATIO	64/2		// Constant for slowness of OOK high level estimator
-#define OOK_EST_LOW_RATIO	1024*100		// Constant for slowness of OOK low level (noise) estimator (very slow)1024
+#define OOK_EST_HIGH_RATIO	64*2			// Constant for slowness of OOK high level estimator
+#define OOK_EST_LOW_RATIO	1024*2		// Constant for slowness of OOK low level (noise) estimator (very slow)1024
 
 // FSK adaptive frequency estimator constants
 #define FSK_DEFAULT_FM_DELTA	6000	// Default estimate for frequency delta
